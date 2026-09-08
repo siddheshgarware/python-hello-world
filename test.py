286da7a058d387af41225b76ba557e39d25f1bc9
siddheshgarware <siddheshgarware@gmail.com>
2026-09-08 20:30:40 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,24 @@
+# (=) is an assignment operator which is used to assign a value to a variable.
+# rhs get stores in lhs (variable)
+# print is a function which is used to print the output on the console.
+
+
+a=5
+b=1000
+sum=a+b
+print("sum of a and b is",sum)
+
+# Q.WAP to find the average of 10, 20 and 30
+
+a=10
+b=20
+c=30
+average=(a+b+c)/3
+print("average of a, b and c is ",average)
+
+
+# interactive mode:- in this mode we can write the code and execute it line by line.(we can use it in shell or command promptor terminal)
+# script mode:- in this mode we can write the code in a file and execute the whole file at once.
+# to startinteractive mode commands in terminal:- python
+# to end interactive mode commands in terminal:- exit() or quit()
+# REPL:- Read Evaluate Print Loop (when we write the code in interactive mode it is repl)
