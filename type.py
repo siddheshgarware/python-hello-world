98e1bac356220261a1ae6caaec122f5a3cff7688
siddheshgarware <siddheshgarware@gmail.com>
2026-09-09 20:10:21 +0530

    Add files via upload

---
+++ type.py
@@ -0,0 +1,39 @@
+#dynaminc typing is a feature of python that allows you to change the type of a variable at runtime. 
+#datatype of a variable is determined by the value assigned to it.
+#datatypes in python are:- int, float, complex, bool, str, list, tuple, set, dict
+
+a=45
+
+print(a,type(a))
+
+b=3.56
+
+print(b,type(b))
+
+c=True
+print(c,type(c))
+
+d="study python"
+print(d,type(d))
+
+k=1+6j
+
+print(k,type(k))
+
+s=None
+print(s,type(s))
+
+
+#WAP to show simple interest if p is 10000,r is 2.5 and t is 5 years
+
+p=10000
+print("datatype of p is ",type(p))
+
+r=2.5 
+print("datatype of r is ",type(r))
+
+t=5
+print("datatype of t is",type(t))
+
+print("simple intrest is ",(p*r*t)/100)
+
